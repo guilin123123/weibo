eec5588ea44fad28d9d693d4e15c85c4e6668f10
guilin <1141768780@qq.com>
2019-04-03 14:48:32 +0000

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.default')
 @section('content')
-  <h1>注册</h1>
+  <div class="offset-md-2 col-md-8">
+    <div class="card">
+      <div class="card-header">
+        <h5>注册</h5>
+      </div>
+       <div class="card-body">
+         <form action="{{route('users.store')}}" method="post">
+          <div class="form-group">
+            <label for="">名称</label>
+            <input type="text" name="name" class="form-control" value="{{old('name')}}">
+          </div>
+           <div class="form-group">
+             <label for="">邮箱</label>
+             <input type="text" name="email" class="form-control" value="{{old('email')}}">
+           </div>
+           <div class="form-group">
+             <label for="">密码</label>
+             <input type="password" name="password" class="form-control">
+           </div>
+           <div class="form-group">
+             <label for="">名称</label>
+             <input type="password" name="password_confirmation" class="form-control">
+           </div>
+           <button type="submit" class="btn btn-primary">注册</button>
+         </form>
+       </div>
+    </div>
+  </div>
 @stop
 @section('title','注册')
