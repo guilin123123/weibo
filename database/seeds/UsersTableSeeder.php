2b4d1d6284f878e019be39f3ada35c2f1f3a4149
guilin <1141768780@qq.com>
2019-04-06 09:05:50 +0000

    管理员删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = '嘻嘻哈哈';
         $user->email = '1141768780@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
