8c9a75bdd397c7a9830133afc163a5effd0fb6ef
guilin <1141768780@qq.com>
2019-04-07 03:03:28 +0000

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -102,13 +102,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'example@qq.com';
-        $name = 'guilin';
         $to = $user->email;
         $subject = '感谢注册,请确认邮箱';
 
-        Mail::send($view,$data,function($message) use ($from,$name,$to,$subject){
-            $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view,$data,function($message) use ($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 
